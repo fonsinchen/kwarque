d113c3fc3fb8681a73e72b26161293236daa3504
Ulf Hermann <ulf_hermann@gmx.net>
2012-04-29 23:07:59 +0200

    fix wraparound

--- public/js/raster.js
+++ public/js/raster.js
@@ -34,23 +34,37 @@ for (var type in raster) {
     }
 }
 
+var wrapAround = function(min, max, val1, val2) {
+    var wrapped = false;
+    if (val1 < min) {
+        wrapped = true;
+        val1 = max - min + val1;
+    }
+    if (val2 > max) {
+        wrapped = true;
+        val2 = min + val2 - max;
+    }
+    if (wrapped && val1 <= val2) {
+        return [min, max];
+    } else {
+        return [val1, val2];
+    }
+}
+
 var exports = {};
 exports.rasterize = function(type, a, b) {
     type = raster[type];
-
-    if (a > type.max) a = type.max;
-    if (a < type.min) a = type.min;
-    if (b > type.max) b = type.max;
-    if (b < type.min) b = type.min;
-    
-    var prevA = a;    
-    var prevB = b;
+    var wrapped = wrapAround(type.min, type.max, Math.floor(a), Math.ceil(b));
+    a = wrapped[0];
+    b = wrapped[1];
+    var prevA = Math.floor(a / type.levels[0]);    
+    var prevB = Math.ceil(b / type.levels[0]);
 
     var i = 1;
     for (; i < type.levels.length; ++i) {
         var offsetA = Math.floor(a / type.levels[i]);
         var offsetB = Math.ceil(b / type.levels[i]);
-        if (offsetB - offsetA > 2) {
+        if (Math.abs(offsetB - offsetA) > 2) {
             var ret = [prevA  * type.levels[i - 1]];
             if (prevB - prevA > 1) {
                 ret.push((prevA + prevB) / 2 * type.levels[i - 1]);
@@ -61,7 +75,14 @@ exports.rasterize = function(type, a, b) {
         prevA = offsetA;
         prevB = offsetB;
     }
-    return {level : i, values : [offsetA  * type.levels[i], offsetB  * type.levels[i]]};
+    if (offsetA === offsetB) {
+        if (offsetB < type.max) {
+            offsetB++;
+        } else {
+            offsetB = type.min;
+        }
+    }
+    return {level : i - 1, values : [offsetA  * type.levels[i - 1], offsetB  * type.levels[i - 1]]};
 }
 
 exports.transform = function(type, intervals, targetLevel) {
@@ -72,11 +93,17 @@ exports.transform = function(type, intervals, targetLevel) {
     var newIntervals = [];
     var offsetA = Math.floor(first / type.levels[targetLevel]);
     var offsetB = Math.ceil(last / type.levels[targetLevel]);
+    var maxOffset = Math.ceil(type.max / type.levels[targetLevel]);
+    var minOffset = Math.floor(type.min / type.levels[targetLevel]);
     for (var i = offsetA; i <= offsetB; ++i) {
         newIntervals.push(type.levels[targetLevel] * i);
+        if (i === maxOffset) i = minOffset;
     }
     return {level : targetLevel, values : newIntervals};
 }
+
+exports.min = function(type) {return raster[type].min;}
+exports.max = function(type) {return raster[type].max;}
 
 K.raster = exports;
 
