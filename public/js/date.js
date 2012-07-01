5e07e7948c1e2cd2aa4aedbc24dd5d0b9c98ffb6
Ulf Hermann <ulf_hermann@gmx.net>
2012-07-01 19:15:26 +0200

    use minus instead of slash for date format

--- public/js/date.js
+++ public/js/date.js
@@ -14,7 +14,7 @@
             return nullpad(date.getHours()) + ':' + nullpad(date.getMinutes());
         },
         format : function(date) {
-            return date.getFullYear() + '/' + K.date.month(date) + '/' +
+            return date.getFullYear() + '-' + K.date.month(date) + '-' +
                 K.date.day(date) + ' ' + K.date.time(date);
         }
     };
@@ -27,27 +27,27 @@
         if (startDate.getFullYear() === endDate.getFullYear()) {
             common += startDate.getFullYear();
             if (startDate.getMonth() === endDate.getMonth()) {
-                common += '/' + K.date.month(startDate);
+                common += '-' + K.date.month(startDate);
                 if (startDate.getDate() === endDate.getDate()) {
-                    common += '/' + K.date.day(startDate);
+                    common += '-' + K.date.day(startDate);
                 } else {
                     start = K.date.day(startDate);
                     end = K.date.day(endDate);
                 }
             } else {
-                start = K.date.month(startDate) + '/' + K.date.day(startDate);
-                end = K.date.month(endDate) + '/' + K.date.day(endDate);
+                start = K.date.month(startDate) + '-' + K.date.day(startDate);
+                end = K.date.month(endDate) + '-' + K.date.day(endDate);
             }
         } else {
             start = startDate.getFullYear();
             end = endDate.getFullYear();
             var yeardiff = end - start;
             if (yeardiff < 10) {
-                start += '/' + K.date.month(startDate);
-                end += '/' + K.date.month(endDate);
+                start += '-' + K.date.month(startDate);
+                end += '-' + K.date.month(endDate);
                 if (yeardiff < 3) {
-                    start += '/' + K.date.day(startDate);
-                    end += '/' + K.date.day(endDate);
+                    start += '-' + K.date.day(startDate);
+                    end += '-' + K.date.day(endDate);
                 }
             }
         }
